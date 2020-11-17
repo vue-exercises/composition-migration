8ee1f02794f109253e6ba763ef6c700ee1679b6c
dedoblege <dedoblege@gmail.com>
2020-11-17 12:09:17 +0100

    migrating to Composition API - app.vue

--- src/App.vue
+++ src/App.vue
@@ -10,23 +10,26 @@ import USER_DATA from './dummy-data.js';
 
 import UserList from './components/users/UserList.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
-
+import { ref } from 'vue';
 export default {
   components: {
     UserList,
-    ProjectsList,
+    ProjectsList
   },
-  data() {
+  setup() {
+    let selectedUser = ref(null);
+    const activeUsers = USER_DATA;
+
+    function selectUser(uid) {
+      selectedUser.value = activeUsers.find(usr => usr.id === uid);
+    }
+
     return {
-      selectedUser: null,
-      activeUsers: USER_DATA,
+      selectedUser,
+      activeUsers,
+      selectUser
     };
-  },
-  methods: {
-    selectUser(uid) {
-      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-    },
-  },
+  }
 };
 </script>
 
